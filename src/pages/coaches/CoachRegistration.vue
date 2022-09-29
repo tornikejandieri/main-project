eaf59cd621b4675bf34c1ede0f0c61b24581baf0
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-29 19:57:34 +0400

    form registration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,11 @@
-<template>REGISTER</template>
+<template>
+  <section>
+    <h2>Register as a coach now</h2>
+    <CoachForm />
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/layout/coaches/CoachForm.vue';
+export default { components: { CoachForm } };
+</script>
