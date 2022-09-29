65b43d986d398640926c6e55e13a6c1e234aee5d
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-29 20:14:31 +0400

    vuex doesnt work again

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,11 +1,19 @@
 <template>
   <section>
     <h2>Register as a coach now</h2>
-    <CoachForm />
+    <CoachForm @save-data="saveData" />
   </section>
 </template>
 
 <script>
 import CoachForm from '../../components/layout/coaches/CoachForm.vue';
-export default { components: { CoachForm } };
+export default {
+  components: { CoachForm },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    },
+  },
+};
 </script>
